653fe2f3e9f879f4f972f075106827bca6403df6
Dhananjay Sudhakar Gatade <DS00359937@techmahindra.com>
2017-11-21 19:25:17 +0530

    Added FirtProject

---
+++ FirstPrj.java
@@ -0,0 +1,12 @@
+
+public class FirstPrj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is my First Project in Java");
+		
+		System.out.println("My name is Dhananjay");
+
+	}
+}
